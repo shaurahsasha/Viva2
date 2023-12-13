fd126ead7946904a35057a0c22d9c4363e416599
shaurahsasha <102988593+shaurahsasha@users.noreply.github.com>
2023-12-13 17:39:56 +0800

    added findmin and round to question 1

--- VIVA2/src/viva2/Viva2Question1.java
+++ VIVA2/src/viva2/Viva2Question1.java
@@ -15,5 +15,39 @@ public class Viva2Question1 {
      */
     public static void main(String args[]) {
         // TODO code application logic here
+        int[] array = {5, 12, 8, 21, 7, 9};
+        int result = complexArrayOperation(array);
+        System.out.println("Result: " + result);
+    }
+
+    public static int complexArrayOperation(int[] arr) {
+        int max = findMax(arr);
+        int min = findMin(arr);
+        int sumofdifference = calculateSumOfDifference(arr);
+        int result = (max * min) - sumofdifference;
+        int finalresult = round(result);
+        return finalresult;
+    }
+
+    public static int findMax(int[] arr) {
+
+    }
+
+    public static int findMin(int[] arr) {
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i ++) {
+            result = Math.min(result, arr[i]);
+        }
+        return result; 
+    }
+
+    public static int calculateSumOfDifference(int[] arr) {
+
+    }
+
+    public static int round(int n) {
+        int result;
+        result = (int)(Math.round((n / 10.0)) * 10);
+        return result;
     }
 }
